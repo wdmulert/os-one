cd0e2763d35e10484772c0b947ee26b15d00f1b8
wdmulert <wmulert@gmail.com>
2017-11-19 17:19:11 -0500

    initial

---
+++ src/main/java/org/zygno/appone/AppOneApplication.java
@@ -0,0 +1,12 @@
+package org.zygno.appone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppOneApplication.class, args);
+	}
+}
