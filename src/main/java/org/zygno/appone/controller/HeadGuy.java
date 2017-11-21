0bb101be7d501511c0e460fca738f8bab282621b
wdmulert <wmulert@gmail.com>
2017-11-20 19:25:19 -0500

    properties

--- src/main/java/org/zygno/appone/controller/HeadGuy.java
+++ src/main/java/org/zygno/appone/controller/HeadGuy.java
@@ -1,5 +1,8 @@
 package org.zygno.appone.controller;
 
+import javax.annotation.Resource;
+
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hg")
 public class HeadGuy {
+	@Resource Environment env;
 
 	@GetMapping("hey")
 	public String rUThere() {
-		return "Uh, Yeah";
+		String prop = env.getProperty("named-environment.raft.val1");
+		return "Uh, Yeah " + prop;
 	}
 }
